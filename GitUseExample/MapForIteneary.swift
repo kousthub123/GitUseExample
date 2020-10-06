2caa20a56f4166fa7bef1a1832e6fad25af5ee06
Koushub Ganugapati <kousthub.ganugapati.738@k12.friscoisd.org>
2020-10-06 13:28:42 -0500

    Add new map-for-itenary feature

---
+++ GitUseExample/MapForIteneary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForIteneary.swift
+//  GitUseExample
+//
+//  Created by Ganugapati, Kousthub S on 10/2/20.
+//  Copyright © 2020 Ganugapati, Kousthub S. All rights reserved.
+//
+
+import Foundation
